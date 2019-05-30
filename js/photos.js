3142682296eca75dc5a8ac001ebb468f41893bc9
Enric Shen <Enric.Shen@boprc.govt.nz>
2019-05-30 15:29:09 +1200

    updated pop ups

--- js/photos.js
+++ js/photos.js
@@ -7,33 +7,29 @@ require([
     // or a proxy is required.
     const url = "http://brcsvweb03.envbop.net:52463/api/photos";
 
-
-    // Paste the url into a browser's address bar to download and view the attributes
-    // in the GeoJSON file. These attributes include:
-    // * mag - magnitude
-    // * type - earthquake or other event such as nuclear test
-    // * place - location of the event
-    // * time - the time of the event
-    // Use out of box popupTemplate function DateString to format time field into a human-readable format
-
-
+    const template = {
+              title: "Photo Info",
+              content: "<b>Name</b>: {name}</br>" +
+                       "<b>Date</b>: {fileCreatedDate}</br>" +
+                       "<b>Link</b>: <a href={url} target='_blank'>Link</a>"
+            };
 
     const renderer = {
       type: "simple",  // autocasts as new SimpleRenderer()
-symbol: {
-type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
-size: 6,
-color: "black",
-outline: {  // autocasts as new SimpleLineSymbol()
-  width: 0.5,
-  color: "white"
-}
-}
-};
+      symbol: {
+        type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+        size: 8,
+        color: "orange",
+        outline: {  // autocasts as new SimpleLineSymbol()
+          width: 0.5,
+          color: "white"
+          }
+        }
+      };
     const geojsonLayer = new GeoJSONLayer({
       url: url,
       copyright: "BOPRC",
-      //popupTemplate: template,
+      popupTemplate: template,
       renderer: renderer //optional
     });
 
@@ -44,8 +40,8 @@ outline: {  // autocasts as new SimpleLineSymbol()
 
     const view = new MapView({
       container: "viewDiv",
-      center: [176, -37],
-      zoom: 5,
+      center: [176.8, -38.1],
+      zoom: 10,
       map: map
     });
   });
